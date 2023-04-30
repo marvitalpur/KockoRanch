3567125e66329ab8579e3343283c43815c11007b
marvitalpur <marvitalpur895@gmail.com>
2023-04-30 17:57:11 +0500

    changes

--- assets/index.js
+++ assets/index.js
@@ -5,7 +5,6 @@ const Assets = {
   Logos: {
     HomeI1: require('./images/BAckgroundMD.png'),
     HomeI2: require('./images/productlg.png'),
-
   },
   products: {
     product1: require('./images/productlg.png'),
@@ -13,6 +12,13 @@ const Assets = {
     product3: require('./images/bottel2.png'),
     product4: require('./images/BAckgroundMD.png'),
   },
+  services: {
+    services1: require('./images/services1.png'),
+    services2: require('./images/Service2.png'),
+    services3: require('./images/treeServices.png'),
+    services4: require('./images/Tractor Repair.png'),
+    services5: require('./images/Pest Control.png'),
+  }
 };
 
 export default Assets;
