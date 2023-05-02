aba824c12ceadaf9f6378242ac88aaa73fdfddb4
kahoodigitalsprojects <75724710+marvitalpur@users.noreply.github.com>
2023-05-02 17:27:36 +0500

    changes

---
+++ assets/svgs/Drawersvg.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import Svg, {
+  G,
+  Path,
+  Line,
+  Defs,
+  Stop,
+  Circle,
+  LinearGradient,
+} from 'react-native-svg';
+const Tractor = () => {
+  return (
+    <>
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="33.379"
+        height="27.004"
+        viewBox="0 0 33.379 27.004">
+        <Path
+          id="Icon_awesome-tractor"
+          data-name="Icon awesome-tractor"
+          d="M26.3,16.737a4.383,4.383,0,1,0,4.383,4.383A4.384,4.384,0,0,0,26.3,16.737Zm0,5.579a1.2,1.2,0,1,1,1.2-1.2A1.2,1.2,0,0,1,26.3,22.316ZM30.286,7.97H27.1v-2a3.193,3.193,0,0,1,.655-1.935.769.769,0,0,0-.066-.971l-1.077-1.2a.811.811,0,0,0-1.23.039A6.384,6.384,0,0,0,23.91,5.967v2h-5.1L16.019,1.452A2.387,2.387,0,0,0,13.82,0H7.173A2.394,2.394,0,0,0,4.782,2.391v7.3a1.084,1.084,0,0,0-1.439.073l-1.55,1.55a1.1,1.1,0,0,0,0,1.55l.252.252a7.557,7.557,0,0,0-.593,1.431H1.1A1.1,1.1,0,0,0,0,15.64v2.192a1.1,1.1,0,0,0,1.1,1.1h.356a7.594,7.594,0,0,0,.593,1.431l-.252.252a1.1,1.1,0,0,0,0,1.55l1.55,1.55a1.1,1.1,0,0,0,1.55,0l.252-.252a7.557,7.557,0,0,0,1.431.593v.356a1.1,1.1,0,0,0,1.1,1.1H9.862a1.1,1.1,0,0,0,1.1-1.1v-.356a7.557,7.557,0,0,0,1.431-.593l.252.252a1.1,1.1,0,0,0,1.55,0l1.55-1.55a1.1,1.1,0,0,0,0-1.55l-.252-.252a7.557,7.557,0,0,0,.593-1.431h.357a1.1,1.1,0,0,0,1.1-1.1v-.3h4.012a5.878,5.878,0,0,1,7.335-1.783l2.531-2.531a1.594,1.594,0,0,0,.467-1.127V9.564A1.593,1.593,0,0,0,30.286,7.97ZM8.767,20.722a3.985,3.985,0,1,1,3.985-3.985A3.985,3.985,0,0,1,8.767,20.722ZM9.863,7.97H7.97V3.188h5.324l2.05,4.782Z"
+          transform="translate(0.75 0.75)"
+          fill="none"
+          stroke="#fff"
+          stroke-width="1.5"
+        />
+      </Svg>
+    </>
+  );
+};
+const Setting = () => {
+  return (
+    <>
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="22.6"
+        height="22.6"
+        viewBox="0 0 22.6 22.6">
+        <G
+          id="Icon_feather-settings"
+          data-name="Icon feather-settings"
+          transform="translate(-0.75 -0.75)">
+          <Path
+            id="Path_1847"
+            data-name="Path 1847"
+            d="M19.255,16.377A2.877,2.877,0,1,1,16.377,13.5,2.877,2.877,0,0,1,19.255,16.377Z"
+            transform="translate(-4.327 -4.327)"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+          />
+          <Path
+            id="Path_1848"
+            data-name="Path 1848"
+            d="M19.147,14.927a1.583,1.583,0,0,0,.316,1.746l.058.058a1.919,1.919,0,1,1-2.714,2.714l-.058-.058a1.6,1.6,0,0,0-2.7,1.132v.163a1.918,1.918,0,0,1-3.836,0V20.6a1.583,1.583,0,0,0-1.036-1.448,1.583,1.583,0,0,0-1.746.317l-.058.058a1.919,1.919,0,1,1-2.714-2.714l.058-.058a1.6,1.6,0,0,0-1.132-2.7H3.418a1.918,1.918,0,0,1,0-3.836H3.5A1.583,1.583,0,0,0,4.953,9.173a1.583,1.583,0,0,0-.317-1.746L4.579,7.37A1.919,1.919,0,1,1,7.293,4.655l.058.058A1.583,1.583,0,0,0,9.1,5.029h.077a1.583,1.583,0,0,0,.959-1.448V3.418a1.918,1.918,0,1,1,3.836,0V3.5a1.6,1.6,0,0,0,2.7,1.132l.058-.058a1.919,1.919,0,1,1,2.714,2.714l-.058.058A1.583,1.583,0,0,0,19.071,9.1v.077a1.583,1.583,0,0,0,1.448.959h.163a1.918,1.918,0,0,1,0,3.836H20.6a1.583,1.583,0,0,0-1.448.959Z"
+            transform="translate(0 0)"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+          />
+        </G>
+      </Svg>
+    </>
+  );
+};
+const Phone = () => {
+  return (
+    <>
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20.531"
+        height="20.567"
+        viewBox="0 0 20.531 20.567">
+        <Path
+          id="Icon_feather-phone-call"
+          data-name="Icon feather-phone-call"
+          d="M14.906,5.129A4.536,4.536,0,0,1,18.49,8.713M14.906,1.5a8.165,8.165,0,0,1,7.213,7.2m-.907,7.24v2.722a1.815,1.815,0,0,1-1.978,1.815,17.955,17.955,0,0,1-7.83-2.785A17.691,17.691,0,0,1,5.96,12.251,17.955,17.955,0,0,1,3.175,4.385,1.815,1.815,0,0,1,4.981,2.407H7.7a1.815,1.815,0,0,1,1.815,1.56,11.649,11.649,0,0,0,.635,2.549,1.815,1.815,0,0,1-.408,1.914L8.591,9.584a14.516,14.516,0,0,0,5.444,5.444l1.152-1.152a1.815,1.815,0,0,1,1.914-.408,11.649,11.649,0,0,0,2.549.635A1.815,1.815,0,0,1,21.211,15.943Z"
+          transform="translate(-2.417 -0.672)"
+          fill="none"
+          stroke="#fff"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1.5"
+        />
+      </Svg>
+    </>
+  );
+};
+const LogOut = () => {
+  return (
+    <>
+      <Svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18.591"
+        height="18.591"
+        viewBox="0 0 18.591 18.591">
+        <G
+          id="Icon_feather-log-out"
+          data-name="Icon feather-log-out"
+          transform="translate(0.75 0.75)">
+          <Path
+            id="Path_1849"
+            data-name="Path 1849"
+            d="M10.2,21.591H6.4a1.9,1.9,0,0,1-1.9-1.9V6.4A1.9,1.9,0,0,1,6.4,4.5h3.8"
+            transform="translate(-4.5 -4.5)"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+          />
+          <Path
+            id="Path_1850"
+            data-name="Path 1850"
+            d="M24,19.995l4.747-4.747L24,10.5"
+            transform="translate(-11.657 -6.702)"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+          />
+          <Path
+            id="Path_1851"
+            data-name="Path 1851"
+            d="M24.894,18H13.5"
+            transform="translate(-7.803 -9.455)"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+          />
+        </G>
+      </Svg>
+    </>
+  );
+};
+const Categories =() =>{
+    return(
+        <>
+
+        </>
+    )
+}
+
+export {Tractor, Phone, LogOut, Setting, Categories};
