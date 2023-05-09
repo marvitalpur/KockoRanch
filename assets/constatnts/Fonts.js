53ca32b5b5768e0ec85402a97f9e8b758f35b915
kahoodigitalsprojects <75724710+marvitalpur@users.noreply.github.com>
2023-05-09 18:07:53 +0500

    chanes

--- assets/constatnts/Fonts.js
+++ assets/constatnts/Fonts.js
@@ -16,12 +16,21 @@ const type = {
 };
 
 const size = {
-  xxxSmall: 10,
-  xxsmall: 11,
-  xxsmall: 13,
+  xxxxSmall: 10,
+  xxxSmall: 11,
+  xxSmall: 13,
   xSmall: 14,
   small: 15,
   normal: 17,
   medium: 18,
   large: 20,
+  xLarge: 24,
+  xxLarge: 30,
+  xxxLarge: 36,
+  xxxxLarge: 40,
+};
+
+export default {
+  type,
+  size,
 };
